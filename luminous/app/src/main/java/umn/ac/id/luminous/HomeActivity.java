ebec079c43ed0df921300d9d7157daee0071997a
andrewdillon8 <58471953+andrewdillon8@users.noreply.github.com>
2021-04-24 10:45:56 +0700

    SignIn, UI
    sign in dengan firebase sudah bisa,
    ada perubahan sedikit terhadap ui

---
+++ luminous/app/src/main/java/umn/ac/id/luminous/HomeActivity.java
@@ -0,0 +1,18 @@
+package umn.ac.id.luminous;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
--- luminous/app/src/main/java/umn/ac/id/luminous/SignUpActivity.java
+++ luminous/app/src/main/java/umn/ac/id/luminous/SignUpActivity.java
@@ -3,23 +3,19 @@ package umn.ac.id.luminous;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
-    private static final String TAG ="EmailPassword";
     private EditText emailField;
     private EditText passField;
     private EditText confirmPass;
@@ -66,15 +62,13 @@ public class SignUpActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            //Log.d(TAG, "createUserWithEmail:success");
-                            Intent intent=new Intent(SignUpActivity.this, MainActivity.class);
+                            Intent intent=new Intent(SignUpActivity.this, SignInActivity.class);
                             startActivity(intent);
 
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
                             Toast.makeText(SignUpActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             updateUI(null);
